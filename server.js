d362d4b835135d197d9d3fd03c8834d098ab64ff
evil3367 <shivaevilking@gmail.com>
2023-12-25 13:02:18 +0530

    done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const {MongoClient} = require("mongodb");
 const Port = process.env.Port || 8000;
+const cors = require("cors")
  const articlesinfo ={
     "learn-react": {
         comments:[],
@@ -13,28 +14,48 @@ const Port = process.env.Port || 8000;
         comments: [],
     },
  }
-
+app.use(cors())
 app.use(express.json({ extended: false}));
-app.get("/api/articles/:name", async (req,res) => {
+const withDB = async(operation,res) => {
     try {
-        const artilename = req.params.name;
+        
     const client = await MongoClient.connect("mongodb://127.0.0.1")
     const db = client.db("mernblog");
-    const articlesinfo = await db
-    .collection("articles")
-    .findOne({name: artilename});
-    res.status(200).json(articlesinfo);
+    await operation(db);
     client.close();
     } catch (error) {
         res.status(500).json({message:"error connecting to database", error})
     }
+}
+app.get("/api/articles/:name", async (req,res) => {
+    withDB(async (db)=>{
+        const artilename = req.params.name;
+   
+    
+    const articlesinfo = await db
+    .collection("articles")
+    .findOne({name: artilename});
+    res.status(200).json(articlesinfo);
+    },res)
+        
+    
     
 })
 app.post('/api/articles/:name/add-comments', (req,res) => {
     const {username, text} = req.body;
     const articleName = req.params.name;
-    articlesinfo[articleName].comments.push({ username, text});
-    res.status(200).send(articlesinfo[articleName]);
+    withDB(async (db) => {
+        const articlesinfo =await db.collection('articles').findOne({ name: articleName});
+        await db.collection('articles').updateOne({name: articleName},
+            {
+                $set: {
+                    comments: articlesinfo.comments.concat({ username, text}),
+                },
+            })
+            const updateArticleInfo = await db.collection("articles")
+            .findOne({ name: articleName});
+            res.status(200).json(updateArticleInfo);
+    },res)
 })
 
 
--- vite-project/src/pages/Article.jsx
+++ vite-project/src/pages/Article.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import articlecontent from './article-content';
 
 // components
+import AddCommentForm from '../components/AddCommentform';
 import Articles from '../components/Articles';
+import CommentList from '../components/commentlist';
+// pages
+import Notfound from './notfound';
 const Article = () => {
   const {name} = useParams();
   const article = articlecontent.find((article) => article.name === name)
-  if(!article) return <h1>Article is not present</h1>
+  const [articleInfo , setarticleInfo] = useState({ comments: []})
+  useEffect(() => {
+    const fetchData = async () =>{
+      const result = await fetch(`http://localhost:8000/api/articles/${name}`)
+      console.log(result);
+      const body = await result.json()
+      console.log(body);
+      setarticleInfo(body);
+      };
+      fetchData();
+    
+  }, [name]);
+  
+  if(!article) return <Notfound/>
   const otherArticles = articlecontent.filter(article => article.name !== name)
   return (
     <>
@@ -15,6 +32,8 @@ const Article = () => {
         {article.title}
       </h1>
       {article.content.map((paragraph, index) => (<p className='mx-auto leading-relaxed text-base mb-4' key={index}>{paragraph}</p>))}
+      <CommentList comments={articleInfo.comments}/>
+      <AddCommentForm articleName={name} setArticleInfo={setarticleInfo} />
       <h1 className='sm:text-2xl text-xl font-bold my-4 text-gray-900'>Other articles
       </h1>
       <div className='flex flex-wrap -m-4'>
